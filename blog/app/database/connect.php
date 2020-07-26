17e09a3bf0a9864d0037d5aaaa79ec714e53b82d
Boluwatife Ajayi <bolu4good@gmail.com>
2020-07-26 10:21:39 +0100

    updated

--- blog/app/database/connect.php
+++ blog/app/database/connect.php
@@ -3,9 +3,9 @@
     $user = 'l8qcac2a3900uc0r';
     $pass = 'yxk6ohjrugyckyhh';
     $db_name = 'xql1yvg1u9vslaxh';
-    $port = '3306';
+    
 
-    $conn = new MySQLi($host, $user, $pass, $db_name,$port);
+    $conn = new MySQLi($host, $user, $pass, $db_name);
 
     if($conn->connect_error){
         die('Database connection error: ' . $conn->connect_error);
