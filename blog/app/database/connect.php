0b6097e47da756b0ae9fbb978cfbdbd789edc0cc
Boluwatife Ajayi <bolu4good@gmail.com>
2020-07-26 10:41:34 +0100

    Update connect.php

--- blog/app/database/connect.php
+++ blog/app/database/connect.php
@@ -1,19 +1,3 @@
 <?php 
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'];
-$username = $dbparts['l8qcac2a3900uc0r'];
-$password = $dbparts['yxk6ohjrugyckyhh'];
-$database = ltrim($dbparts['xql1yvg1u9vslaxh'],'/');
-    
-
-    $conn = new mysqli($hostname, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection was successfully established!";
-
+echo "do you work";
 ?>
