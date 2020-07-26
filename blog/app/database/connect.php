7a37094e62177d09df98a8329940f4ad12ae7476
Boluwatife Ajayi <bolu4good@gmail.com>
2020-07-26 10:17:07 +0100

    updated credentials for MySQL database

--- blog/app/database/connect.php
+++ blog/app/database/connect.php
@@ -1,10 +1,11 @@
 <?php 
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db_name = 'blog';
+    $host = 'vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+    $user = 'l8qcac2a3900uc0r';
+    $pass = 'yxk6ohjrugyckyhh';
+    $db_name = 'xql1yvg1u9vslaxh';
+    $port = '3306';
 
-    $conn = new MySQLi($host, $user, $pass, $db_name);
+    $conn = new MySQLi($host, $user, $pass, $db_name,$port);
 
     if($conn->connect_error){
         die('Database connection error: ' . $conn->connect_error);
