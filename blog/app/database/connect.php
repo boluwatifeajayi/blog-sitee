5723bd462a7fbca4a6550372f05f1aa57b08e56c
Boluwatife Ajayi <bolu4good@gmail.com>
2020-07-27 01:50:10 +0100

    Update connect.php

--- blog/app/database/connect.php
+++ blog/app/database/connect.php
@@ -1,14 +1,15 @@
 <?php 
-$hostname = 'vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-$username = 'l8qcac2a3900uc0r';
-$password = 'yxk6ohjrugyckyhh';
-$database = 'xql1yvg1u9vslaxh';
+    $host = 'vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+    $user = 'l8qcac2a3900uc0r';
+    $pass = 'yxk6ohjrugyckyhh';
+    $db_name = 'xql1yvg1u9vslaxh';
 
-    // Create connection
-$conn = new mysqli($hostname, $username, $password, $database);
+    $conn = new MySQLi($host, $user, $pass, $db_name);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection was successfully hbhibhbib established!";
+    if($conn->connect_error){
+        die('Database connection error: ' . $conn->connect_error);
+    }else{
+        echo "Db connection successful";
+    }
+
+?>
