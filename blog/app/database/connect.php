d408d664bc6330e74fec203f9f1146fa8623c17c
Boluwatife Ajayi <bolu4good@gmail.com>
2020-07-27 01:36:27 +0100

    Update connect.php

--- blog/app/database/connect.php
+++ blog/app/database/connect.php
@@ -11,4 +11,4 @@ $conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connection was successfully established!";
+echo "Connection was successfully hbhibhbib established!";
